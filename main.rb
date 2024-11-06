c5a4e2e1ed2f86aae22592660ff3314bc2d1e32e
shackfu1 <129890135+shackfu1@users.noreply.github.com>
2024-11-05 16:04:35 -0800

    Add files via upload

---
+++ main.rb
@@ -0,0 +1,17 @@
+class Student
+
+  MAX_COURSES = 5
+  
+  def remove_from_schedule(course, course_list)
+    course_list.remove(course)
+  end
+
+  def add_to_schedule(course, course_list)
+    max_courses = course_list.maximum_number_of_courses
+    if max_courses < MAX_COURSES
+        course_list.add(course)
+    end
+  end
+
+end
+
